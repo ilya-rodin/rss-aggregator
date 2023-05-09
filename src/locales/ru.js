95644049dcfb7cf86cba426df7bc86285c37582e
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-09 16:58:35 +0300

    add locales

---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    success: 'RSS успешно загружен',
+    errors: {
+      repeatUrl: 'RSS уже существует',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+    },
+  },
+};
