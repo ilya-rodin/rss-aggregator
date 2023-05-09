98d77f70db29fb0a2580803ab7ff225e34f40e06
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-09 17:18:34 +0300

    add form reset

--- src/view.js
+++ src/view.js
@@ -1,19 +1,26 @@
+function resetForm(form) {
+  form.reset();
+  form.focus();
+}
+
 function setFeedbackError(errorCode, elements, i18n) {
-  const { feedback, input } = elements;
+  const { feedback, input, form } = elements;
 
   input.classList.add('is-invalid');
   feedback.classList.add('text-danger');
   feedback.classList.remove('text-success');
   feedback.textContent = i18n.t(`errors.${errorCode}`);
+  resetForm(form);
 }
 
 function setFeedbackSuccess(elements, i18n) {
-  const { feedback, input } = elements;
+  const { feedback, input, form } = elements;
 
   input.classList.remove('is-invalid');
   feedback.classList.remove('text-danger');
   feedback.classList.add('text-success');
   feedback.textContent = i18n.t('success');
+  resetForm(form);
 }
 
 function render(elements, state, i18n) {
