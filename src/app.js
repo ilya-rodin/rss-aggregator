0d84920b41149db80e699e004afd416dd57bd717
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-11 19:50:27 +0300

    add post adding

--- src/app.js
+++ src/app.js
@@ -1,63 +1,144 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
 import onChange from 'on-change';
-import resources from './locales/index.js';
-import locale from './locales/locale.js';
+import axios from 'axios';
+import { uniqueId } from 'lodash';
+import ru from './locales/ru.js';
 import render from './view.js';
+import parse from './parser.js';
+
+const REFRESH_TIME = 5000;
 
 function validateUrl(url, otherUrls) {
-  const schema = yup
-    .string()
-    .trim()
-    .url()
-    .notOneOf(otherUrls);
+  const schema = yup.string().trim().url().notOneOf(otherUrls);
   return schema.validate(url);
 }
 
-function runApp(i18n) {
-  const state = {
-    form: {
-      status: 'filling',
-      error: null,
-    },
-    feeds: [],
-  };
+function getAllOriginsURL(url) {
+  const allOriginsURL = new URL('https://allorigins.hexlet.app/get');
+  allOriginsURL.searchParams.set('disableCache', 'true');
+  allOriginsURL.searchParams.set('url', url);
+  return allOriginsURL;
+}
 
-  const elements = {
-    form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    feedback: document.querySelector('.feedback'),
-    submitButton: document.querySelector('button[type="submit"]'),
-  };
+function addPosts(state, posts) {
+  const newPostsWithId = posts.map((post) => ({ ...post, id: uniqueId() }));
+  state.content.posts = [...newPostsWithId, ...state.content.posts];
+}
 
-  const watchedState = onChange(state, () => render(elements, state, i18n));
+function refreshFeeds(state) {
+  const { feeds } = state.content;
+  const oldPosts = state.content.posts;
 
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
+  const promises = feeds.map((feed) => {
+    const { link } = feed;
+    const allOriginsURL = getAllOriginsURL(link);
+    return axios.get(allOriginsURL).then((response) => {
+      const rss = response.data.contents;
+      const { posts } = parse(rss);
+      const oldLinks = oldPosts.map((post) => post.link);
+      const newPosts = posts.filter((post) => !oldLinks.includes(post.link));
+      if (newPosts.length > 0) {
+        addPosts(state, newPosts);
+      }
+    });
+  });
 
-    validateUrl(url, watchedState.feeds)
-      .then(() => {
-        watchedState.form.status = 'success';
-        watchedState.feeds.push(url);
-      })
-      .catch((error) => {
-        watchedState.form.error = error.message;
-        watchedState.form.status = 'error';
-      });
+  Promise.all(promises).finally(() => {
+    setTimeout(() => refreshFeeds(state), REFRESH_TIME);
   });
 }
 
-export default () => {
-  const i18n = i18next.createInstance();
+function runApp() {
+  const i18nInstance = i18next.createInstance();
+  i18nInstance
+    .init({
+      lng: 'ru',
+      debug: false,
+      resources: {
+        ru,
+      },
+    })
+    .then((i18nT) => {
+      const initialState = {
+        form: {
+          status: 'filling',
+          error: null,
+        },
+        content: {
+          feeds: [],
+          posts: [],
+        },
+        ui: {
+          modalId: null,
+          visitedIds: [],
+        },
+      };
 
-  i18n.init({
-    lng: 'ru',
-    resources,
-  })
-    .then(() => {
-      yup.setLocale(locale);
-      runApp(i18n);
+      const elements = {
+        form: document.querySelector('.rss-form'),
+        feedback: document.querySelector('.feedback'),
+        input: document.querySelector('#url-input'),
+        btn: document.querySelector('button[type="submit"]'),
+        posts: document.querySelector('.posts'),
+        feeds: document.querySelector('.feeds'),
+      };
+
+      yup.setLocale({
+        mixed: { notOneOf: 'existingFeed' },
+        string: { url: 'invalidLink', required: 'emptyForm' },
+      });
+
+      const watchedState = onChange(
+        initialState,
+        render(elements, initialState, i18nT)
+      );
+
+      refreshFeeds(watchedState);
+
+      elements.form.addEventListener('input', () => {
+        watchedState.form.status = 'filling';
+        watchedState.form.error = null;
+      });
+
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(elements.form);
+        const url = formData.get('url');
+        const links = watchedState.content.feeds.map(({ link }) => link);
+
+        validateUrl(url, links)
+          .then((link) => {
+            watchedState.form.status = 'sending';
+            const allOriginsURL = getAllOriginsURL(link);
+            return axios.get(allOriginsURL);
+          })
+          .then((response) => {
+            const rssXML = response.data.contents;
+            const { feed, posts } = parse(rssXML);
+            watchedState.content.feeds.push({
+              ...feed,
+              id: uniqueId(),
+              link: url,
+            });
+            addPosts(watchedState, posts);
+            watchedState.form.status = 'finished';
+          })
+          .catch((error) => {
+            const errorKey = error.message;
+            watchedState.form.error = errorKey;
+            watchedState.form.status = 'failed';
+          });
+      });
+
+      elements.posts.addEventListener('click', (e) => {
+        const { id } = e.target.dataset;
+        if (id && !initialState.ui.visitedIds.includes(id)) {
+          watchedState.ui.visitedIds.push(id);
+        }
+      });
     });
-};
+}
+
+export default runApp;
--- src/view.js
+++ src/view.js
@@ -3,42 +3,183 @@ function resetForm(form) {
   form.focus();
 }
 
-function setFeedbackError(errorCode, elements, i18n) {
+function renderFeeds(elements, state, i18nT) {
+  const { feeds } = elements;
+  feeds.innerHTML = '';
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = i18nT('feeds');
+
+  cardBody.append(cardTitle);
+
+  const cardList = document.createElement('ul');
+  cardList.classList.add('list-group', 'border-0', 'rounder-0');
+
+  state.content.feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+    const liTitle = document.createElement('h3');
+    liTitle.classList.add('h6', 'm-0');
+    liTitle.textContent = feed.title;
+
+    const liDescription = document.createElement('p');
+    liDescription.classList.add('m-0', 'small', 'text-black-50');
+    liDescription.textContent = feed.description;
+
+    li.append(liTitle, liDescription);
+    cardList.prepend(li);
+  });
+
+  card.append(cardBody, cardList);
+  feeds.append(card);
+}
+
+function renderPosts(elements, state, i18nT) {
+  const { posts } = elements;
+  posts.innerHTML = '';
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = i18nT('posts');
+
+  cardBody.append(cardTitle);
+
+  const cardList = document.createElement('ul');
+  cardList.classList.add('list-group', 'border-0', 'rounder-0');
+
+  state.content.posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0',
+    );
+
+    const link = document.createElement('a');
+    link.href = post.link;
+    link.classList.add(state.ui.visitedIds.includes(post.id)
+      ? ('fw-normal', 'link-secondary')
+      : 'fw-bold');
+
+    link.setAttribute('data-id', post.id);
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.textContent = post.title;
+
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    btn.setAttribute('data-id', post.id);
+    btn.textContent = i18nT('preview');
+
+    li.append(link, btn);
+    cardList.append(li);
+  });
+
+  card.append(cardBody, cardList);
+  posts.append(card);
+}
+
+function handleFormFilling(elements) {
+  const { feedback, input } = elements;
+
+  input.classList.remove('is-invalid');
+  feedback.classList.remove('text-danger');
+  feedback.classList.remove('text-success');
+  feedback.textContent = null;
+}
+
+function handleFormSending(elements) {
+  const { btn, input } = elements;
+  btn.disabled = true;
+  input.disabled = true;
+}
+
+function handleFormError(errorCode, elements, i18nT) {
   const { feedback, input, form } = elements;
 
   input.classList.add('is-invalid');
   feedback.classList.add('text-danger');
   feedback.classList.remove('text-success');
-  feedback.textContent = i18n.t(`errors.${errorCode}`);
+  feedback.textContent = i18nT(`errors.${errorCode}`);
   resetForm(form);
 }
 
-function setFeedbackSuccess(elements, i18n) {
-  const { feedback, input, form } = elements;
+function handleFormSuccess(elements, i18nT) {
+  const {
+    feedback, btn, input, form
+  } = elements;
+
+  btn.disabled = false;
+  input.disabled = false;
 
   input.classList.remove('is-invalid');
   feedback.classList.remove('text-danger');
   feedback.classList.add('text-success');
-  feedback.textContent = i18n.t('success');
+  feedback.textContent = i18nT('success');
+
   resetForm(form);
 }
 
-function render(elements, state, i18n) {
+function handleFormStatus(elements, state, i18nT) {
   switch (state.form.status) {
     case 'filling':
+      handleFormFilling(elements);
       break;
 
-    case 'success':
-      setFeedbackSuccess(elements, i18n);
+    case 'sending':
+      handleFormSending(elements);
       break;
 
-    case 'error':
-      setFeedbackError(state.form.error, elements, i18n);
+    case 'finished':
+      handleFormSuccess(elements, i18nT);
+      break;
+
+    case 'failed':
+      handleFormError(state.form.error, elements, i18nT);
       break;
 
     default:
       throw new Error('Unknown status');
   }
 }
+
+const render = (elements, state, i18nT) => (path) => {
+  switch (path) {
+    case 'form.error':
+    case 'form.status':
+      handleFormStatus(elements, state, i18nT);
+      break;
+
+    case 'content.feeds':
+      renderFeeds(elements, state, i18nT);
+      break;
+
+    case 'content.posts':
+    case 'ui.visitedIds':
+      renderPosts(elements, state, i18nT);
+      break;
+
+    default:
+      throw new Error('unknown path');
+  }
+};
 
 export default render;
