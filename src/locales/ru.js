a855458b1bf9876e0acca5ddd4b8e800d5a85bc4
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-11 19:51:19 +0300

    move all locales to ru.js

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,8 +2,15 @@ export default {
   translation: {
     success: 'RSS успешно загружен',
     errors: {
-      repeatUrl: 'RSS уже существует',
-      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidLink: 'Ссылка должна быть валидным URL',
+      existingFeed: 'RSS уже существует',
+      parsingError: 'Ресурс не содержит валидный RSS',
+      emptyForm: 'Не должно быть пустым',
+      NetworkError: 'Ошибка сети',
+      defaultError: 'Что-то пошло не так...',
     },
+    preview: 'Просмотр',
+    feeds: 'Фиды',
+    posts: 'Посты',
   },
 };
