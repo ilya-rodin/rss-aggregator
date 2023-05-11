1a2b462f5c5b5efca1db93931a71a8514daa272e
Ilya Rodin <i1.rodin@yandex.ru>
2023-05-11 19:51:31 +0300

    add rss parser

---
+++ src/parser.js
@@ -0,0 +1,31 @@
+function parse(xml) {
+  const domParser = new DOMParser();
+  const parsedDOM = domParser.parseFromString(xml, 'application/xml');
+
+  const parsingError = parsedDOM.querySelector('parsererror');
+  // if (parsingError) throw new Error('parsingError');
+  if (parsingError) console.log('Error parsing');
+
+  const channel = parsedDOM.querySelector('channel');
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const feed = { title, description };
+
+  const items = parsedDOM.querySelectorAll('item');
+  const itemsArray = Array.from(items);
+
+  const posts = itemsArray.map((item) => {
+    const postLink = item.querySelector('link').textContent;
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return {
+      link: postLink,
+      title: postTitle,
+      description: postDescription,
+    };
+  });
+
+  return { feed, posts };
+}
+
+export default parse;
